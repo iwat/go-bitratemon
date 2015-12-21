a2884c9c061631764e8dcf68a2a5dfa4ae90289a
Chaiwat Shuetrakoonpaiboon <iwatth@gmail.com>
2015-12-21 15:22:06 +0700

    Add MIT license header to each source files.

--- main.go
+++ main.go
@@ -1,3 +1,10 @@
+// bitratemon - Monitor bitrate of given video stream to see how it swings
+
+// Copyright (c) 2015 Chaiwat Shuetrakoonpaiboon. All rights reserved.
+//
+// Use of this source code is governed by a MIT license that can be found in
+// the LICENSE file.
+
 package main
 
 import (
